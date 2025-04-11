a91c7681db0a57dbc918e57011f6aa85562e64ec
maxplush <maxabeplush@gmail.com>
2025-04-11 10:37:47 -0700

    updated changes

--- load_tweets.py
+++ load_tweets.py
@@ -101,8 +101,6 @@ def insert_tweet(connection,tweet):
     if res.first() is not None:
         return
 
-    if connection.in_transaction():
-        connection.commit()
 
     # insert tweet within a transaction;
     # this ensures that a tweet does not get "partially" loaded
