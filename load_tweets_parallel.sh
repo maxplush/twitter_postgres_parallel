4701a61e8f2e53d640b4824f0864473d9a342213
maxplush <maxabeplush@gmail.com>
2025-04-21 14:34:39 -0700

    first attempt at modifying the load tweets parallel file

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,13 +6,18 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time echo "$files" | parallel ./load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+time echo "$files" | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:1629/postgres --inputs={}
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
+
+time echo "$files" | parallel python3 load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1729/postgres --inputs={}
+
